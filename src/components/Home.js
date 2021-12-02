5b91b49e472db16577a70eaee9111c4c8a5583f8
CodingWithEnthusiasm <95029999+CodingWithEnthusiasm@users.noreply.github.com>
2021-12-03 02:27:41 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
 
 function Home() {
     return (
@@ -25,8 +26,7 @@ postition: absolute;
 top: 0;
 left: 0;
 right: 0;
-bottom: 0;
-z-index: -1;
+bottom: 0;z-index: -1;
 
 }
 
