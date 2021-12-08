a7f2a9d0434ee8c0fc01973b1f8f25e2cf1f0019
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2021-12-08 12:39:42 +0100

    Move header to layout folder

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import classes from "./Header.module.css";
+
+function Header() {
+  return (
+    <header className= {classes.header}>
+      <img  className = {classes.logo} src="/images/logo.svg" />
+      <nav className={classes.nav}>
+        <a >
+            <Link to="/">
+            <img src="/images/home-icon.svg"></img>
+            <span> HOME </span>
+            </Link>
+        </a>
+        
+        <a>
+            <Link to='/watchList'>
+          <img src="/images/watchlist-icon.svg"></img>
+          <span>WATCHLIST</span>
+          </Link>
+        </a>
+
+        <a>
+          <Link to='/Movie'>
+            <img src="/images/movie-icon.svg"></img>
+            <span>MOVIES</span>
+          </Link>
+        </a>
+
+        <a>
+          <Link to='/Series'>
+            <img src="/images/series-icon.svg"></img>
+            <span>SERIES</span>
+          </Link>
+        </a>
+     
+      </nav>
+      <img className={classes.UserImg} src="https://wimg.ruan8.com/uploadimg/image/20190131/20190131130305_65861.jpg" />
+   </header>
+  );
+}
+
+export default Header;
+
+
+
+
+  
+ 
+   
+
+   
+
+
+
