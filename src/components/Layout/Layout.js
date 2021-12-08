927b884083ef135605fcc852e2024ed84d8ed943
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2021-12-08 12:41:53 +0100

    Add new files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import Header from "./Header"
+import Home from "../Home"
+import classes from "./Layout.module.css"
+function Layout (){
+    return <div>
+        <Header/>
+    </div>
+}
+
+export default Layout 
