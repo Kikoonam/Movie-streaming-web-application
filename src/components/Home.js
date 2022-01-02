ce5af396138aa660bf5c965ee1d0003306768a7b
CodingWithEnthusiasm <95029999+CodingWithEnthusiasm@users.noreply.github.com>
2022-01-02 22:27:42 +0300

    New firebase line

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,10 @@ import { useDispatch, useSelector } from "react-redux";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { auth, provider } from "firebase/app";
 import { initializeApp } from "firebase/app"
 import { setMovies } from "../features/movie/movieSlice";
 import { selectUserName } from "../features/user/userSlice";
-
 
 
 const firebaseConfig = {
@@ -53,6 +53,7 @@ const Home = (props) => {
                         newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
                         break;
 
+                        
                     case "original":
                         originals = [...originals, { id: doc.id, ...doc.data() }];
                         break;
@@ -87,21 +88,20 @@ const Home = (props) => {
 };
 
 const Container = styled.main`
-  position: relative;
-  min-height: calc(100vh - 250px);
-  overflow-x: hidden;
-  display: block;
-  top: 72px;
-  padding: 0 calc(3.5vw + 5px);
-  &:after {
-    background: url("/images/home-background.png") center center / cover
-      no-repeat fixed;
-    content: "";
-    position: absolute;
-    inset: 0px;
-    opacity: 1;
-    z-index: -1;
-  }
-`;
-
+     position: relative;
+                        min - height: calc(100vh - 250px);
+                        overflow - x: hidden;
+                        display: block;
+                        top: 72px;
+                        padding: 0 calc(3.5vw + 5px);
+  &: after {
+                            background: url("/images/home-background.png") center center / cover
+                            no - repeat fixed;
+                            content: "";
+                            position: absolute;
+                            inset: 0px;
+                            opacity: 1;
+                            z - index: -1;
+                        }
+                        `
 export default Home;
