3e567a3bfacf564b64696f729403c55bb6a604ee
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2022-01-03 08:44:22 +0100

    Some small changes

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,73 +1,76 @@
-/*import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectMovie } from "../features/movie/movieSlice";
+import React from "react";
+import styled from "styled-components";
 
-const Movies = (props) => {
-  const movies = useSelector(selectMovie);
-
+function Movies() {
   return (
     <Container>
-      <h1>ALL MOVIES</h1>
+      <h2>MOVIES</h2>
       <Content>
-        {movies &&
-          movies.map((movie, key) => (
-            <Wrap key={key}>
-              {movie.id}
-              <Link to={`/detail/` + movie.id}>
-                <img src={movie.cardImg} alt={movie.title} />
-              </Link>
-            </Wrap>
-          ))}
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
       </Content>
+      
     </Container>
   );
-};
+}
 
-const Container = styled.div`
-  padding: 0 0 26px;
-`;
+export default Movies;
+
+const Container = styled.div`min-height: calc(100vh - 70px);
+padding: 0 calc(3.5vw + 5px);
+position: relative;
+overflow-x:hidden;
+background: url("/images/home-background.png") center center / cover 
+no-repeat fixed;
+&:before {
+content: "";
+postition: absolute;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+z-index: -1;`;
 
 const Content = styled.div`
   display: grid;
   grid-gap: 25px;
-  gap: 25px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
 `;
 
 const Wrap = styled.div`
-  padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-  cursor: pointer;
   overflow: hidden;
-  position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  cursor: pointer;
   border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: 0px 8px 14px;
+  color: #000000;
+  -webkit-transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  transition: transform 0.4s;
   img {
-    inset: 0px;
-    display: block;
+    width: 100%;
     height: 100%;
     object-fit: cover;
-    opacity: 1;
-    position: absolute;
-    transition: opacity 500ms ease-in-out 0s;
-    width: 100%;
-    z-index: 1;
-    top: 0;
   }
+
   &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    -webkit-transform: scale(1.05);
+    -moz-transform: scale(1.05);
+    -o-transform: scale(1.05);
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
+    box-shadow: 0px 8px 14px;
+    -webkit-transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+    transition: transform 0.4s;
   }
 `;
 
-export default Movies;
-*/
