98ac254659d34947f0e5bd310f07a431cb5c32fd
jinshira <95095827+jinshira@users.noreply.github.com>
2022-01-03 17:41:27 +0100

    Update Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,6 +1,5 @@
 import {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import React from 'react';
 import styled from 'styled-components';
 import db from "../firebase";
 
